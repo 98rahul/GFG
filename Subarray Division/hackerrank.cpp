137e097c6db7275a3cc399c599de7dce33f03ffc
RAHUL RAJ <71221556+98rahul@users.noreply.github.com>
2021-07-12 05:27:41 +0530

    Update hackerrank.cpp

--- Subarray Division/hackerrank.cpp
+++ Subarray Division/hackerrank.cpp
@@ -1,4 +1,7 @@
-/* using sliding window technique O(N) */
+/* using sliding window technique O(N) 
+very useful see how it changes the time complexity  
+
+*/
 
 int birthday(vector<int> s, int d, int m) {
    int max_sum=0; int count =0;
@@ -11,10 +14,34 @@ int birthday(vector<int> s, int d, int m) {
    
    int window_sum= max_sum;
    for(;i<s.size();i++)
-   {
+   { 
        window_sum+=s[i]-s[i-m];
-    //    max_sum=max(max_sum, window_sum);
+   
     if(window_sum==d)count++;
    }
    return count;
 }
+
+/* solution in O(N*M)
+ int x=m;
+    int count =0;
+    for(int start=0; start<s.size();start++)
+    { 
+        int sum=0;int e=start;
+        
+        while(x )
+           {
+            sum+=s[e];
+            x--;
+            e++;
+        }
+        if(sum==d)
+         {
+             count++; 
+         }
+         x=m;
+    }
+  
+
+    return count;
+    */
